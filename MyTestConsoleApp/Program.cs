c4029de23e2efb8b9abb9dc7478723c54b6fff74
DESKTOP-MQM5KDN\JimmyCHU <jimny1014@gmail.com>
2024-03-25 09:40:21 +0800

    Add FW update test.

--- MyTestConsoleApp/Program.cs
+++ MyTestConsoleApp/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace MyTestConsoleApp
+﻿using MyTestConsoleApp.MyTestClass;
+
+namespace MyTestConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            new AutoItX3Test();
         }
     }
 }
