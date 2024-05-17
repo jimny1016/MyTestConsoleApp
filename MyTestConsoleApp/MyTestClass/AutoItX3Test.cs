c762ccba0d4debda4cad07ce671f586ddb30ad0c
DESKTOP-MQM5KDN\JimmyCHU <jimny1014@gmail.com>
2024-05-17 16:34:53 +0800

    Add update Y70 FW function.

--- MyTestConsoleApp/MyTestClass/AutoItX3Test.cs
+++ MyTestConsoleApp/MyTestClass/AutoItX3Test.cs
@@ -1,10 +1,42 @@
 ﻿using AutoIt;
+using System.Runtime.InteropServices;
 
 namespace MyTestConsoleApp.MyTestClass
 {
     internal class AutoItX3Test
     {
         public AutoItX3Test()
+        {
+            Y70UpdateFW();
+        }
+
+        private void Y70UpdateFW()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Y70UpdateFiles", "RTDCustomerTool_V1.1_2555", "RCDTool.exe");
+            AutoItX.Run(filePath, "", AutoItX.SW_NORMAL);
+            AutoItX.WinWait("Realtek - Monitor Customer Tool V1.1 Beta3", "", 3);
+            AutoItX.ControlClick("Realtek - Monitor Customer Tool V1.1 Beta3", "", "[CLASS:SysTreeView32; INSTANCE:1]", numClicks:5, x:34, y:27);
+
+            AutoItX.WinMove("Realtek - Monitor Customer Tool V1.1 Beta3", "", 0, 0, 1200, 900);
+            AutoItX.MouseClick("left", 150, 90, 1, 0);
+            AutoItX.MouseClick("left", 750, 90, 3, 0);
+
+            AutoItX.WinActivate("開啟");
+            var title = AutoItX.WinGetTitle("[REGEXPTITLE:(開啟|Open)]");
+            AutoItX.ControlSetText("", "", "[CLASS:ToolbarWindow32; INSTANCE:3]", "C:\\path\to\\your\\file");
+
+            AutoItX.Sleep(200);
+            var fwFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Y70UpdateFiles", "WH069_3840_1100_xxxx_A2FC_0513_V16.bin");
+            AutoItX.ControlSetText($"{title}", "", "[CLASS:Edit; INSTANCE:1]", fwFilePath);
+            AutoItX.Send("{ENTER}");
+            AutoItX.Sleep(100);
+            AutoItX.ControlClick("Realtek - Monitor Customer Tool V1.1 Beta3", "", "[CLASS:Button; INSTANCE:73]",numClicks: 1);
+            //AutoItX.WinSetState("Realtek - Monitor Customer Tool V1.1 Beta3", "", 0);
+            //AutoItX.WinWait("Realtek - Monitor Customer Tool V1.1 Beta3", "", 30);
+            //AutoItX.WinClose("Realtek - Monitor Customer Tool V1.1 Beta3");
+        }
+
+        private void KeebUpdateFW()
         {
             var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Firmwares", "K2862_50343_64LQFP_HV133_20240308_2439H.exe");
 
@@ -14,6 +46,6 @@ namespace MyTestConsoleApp.MyTestClass
             AutoItX.ControlClick("Holtek TinyPorgrammer V1.0.0", "", "[CLASS:Button; TEXT:Connect]");
             AutoItX.WinWait("Holtek TinyProgrammer V1.0.0", "", 10);
             AutoItX.WinClose("Holtek TinyPorgrammer V1.0.0");
-         }
+        }
     }
 }
--- MyTestConsoleApp/Program.cs
+++ MyTestConsoleApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using MyTestConsoleApp.MyTestClass;
+using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using WindowsDisplayAPI;
 
 namespace MyTestConsoleApp
@@ -8,7 +11,36 @@ namespace MyTestConsoleApp
     {
         static void Main(string[] args)
         {
+            if (!IsAdministrator())
+            {
+                Console.WriteLine("restarting as admin");
+                StartAsAdmin(Assembly.GetExecutingAssembly().Location);
+                return;
+            }
+
             _ = new AutoItX3Test();
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static void StartAsAdmin(string fileName)
+        {
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                }
+            };
+
+            proc.Start();
         }
     }
 }
