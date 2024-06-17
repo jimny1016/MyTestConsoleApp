a8f71963af3a33d457fbfd61a3877cb823873120
DESKTOP-MQM5KDN\JimmyCHU <jimny1014@gmail.com>
2024-06-17 15:27:17 +0800

    Clean up code.

--- MyTestConsoleApp/MyTestClass/DDCCIControl.cs
+++ MyTestConsoleApp/MyTestClass/DDCCIControl.cs
@@ -1,5 +1,4 @@
-﻿using System.Management;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text;
 using Vanara.PInvoke;
 using static MyTestConsoleApp.MyTestClass.Win32.Dxva2;
@@ -37,11 +36,6 @@ public class DDCCIControl
                     {
                         continue;
                     }
-                    var monitorFriendlyName = GetMonitorFriendlyName(monitorInfo.szDevice);
-                    var monitorResolution = GetMonitorResolution(monitorInfo.szDevice);
-
-                    Console.WriteLine($"Friendly Name: {monitorFriendlyName}");
-                    Console.WriteLine($"Resolution: {monitorResolution.Width}x{monitorResolution.Height}");
 
                     // Set the monitor RGB values
                     SetMonitorRGB(physicalMonitor.hPhysicalMonitor, 0x0A, 0x0A, 0x0A);
@@ -74,39 +68,6 @@ public class DDCCIControl
         {
             Console.WriteLine("Failed to set blue gain.");
         }
-    }
-
-    private string GetMonitorFriendlyName(string deviceId)
-    {
-        // 使用WMI查詢Win32_DesktopMonitor，這裡我們假設友好名稱是顯示設備的描述
-        using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DesktopMonitor"))
-        {
-            foreach (ManagementObject mo in searcher.Get())
-            {
-                if (mo["PNPDeviceID"] != null && mo["PNPDeviceID"].ToString().Contains(deviceId.Substring(4)))
-                {
-                    return mo["Name"]?.ToString();
-                }
-            }
-        }
-        return null;
-    }
-
-    private (int Width, int Height) GetMonitorResolution(string deviceId)
-    {
-        using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController"))
-        {
-            foreach (ManagementObject mo in searcher.Get())
-            {
-                if (mo["PNPDeviceID"] != null && mo["PNPDeviceID"].ToString().Contains(deviceId.Substring(4)))
-                {
-                    int width = int.Parse(mo["CurrentHorizontalResolution"].ToString());
-                    int height = int.Parse(mo["CurrentVerticalResolution"].ToString());
-                    return (width, height);
-                }
-            }
-        }
-        return (0, 0);
     }
 
     private bool CheckVCPFeatureSupport(IntPtr hPhysicalMonitor, byte vcpCode)
